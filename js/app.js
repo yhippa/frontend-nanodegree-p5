14e2d1912d74a5b12da8872baeb3a93a8b858bf9
Richard Yhip <Richard Yhip>
2017-01-02 22:35:55 -0500

    refactored code to include eventful API data

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const FOURSQUARE_CLIENT_ID = "OYNSAXXQEBFWL2V3VVXJNZTB5RWOTYUMMKHUEGG5CVS44BKF";
 const FOURSQUARE_CLIENT_SECRET = "KSS3BGAWCRRQBZPX1F20ZL5WY2BIRRBJPSRZBNP3VJAUHN0C";
 const FOURSQUARE_API_VERSION = "20161221";
 const GOOGLE_MAPS_JS_API_KEY = "AIzaSyApoU2dpC25z2Z45WKMGV16kdTatObgr4M";
+const EVENTFUL_API_KEY = "t5wrJZZBq3bMtzvG";
 
 function AppViewModel() {
     var self = this;
@@ -11,8 +12,38 @@ function AppViewModel() {
 
 ko.applyBindings(new AppViewModel());
 
+// http://api.eventful.com/json/events/search?app_key=t5wrJZZBq3bMtzvG&where=38.857481,-77.196756&within=25 
 // https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=FOURSQUARE_CLIENT_IDF&client_secret=FOURSQUARE_CLIENT_SECRET&v=20161221
 // https://maps.googleapis.com/maps/api/js?key=AIzaSyApoU2dpC25z2Z45WKMGV16kdTatObgr4M&callback=initMap
+function getEventfulEvents(lat, lng, map) {
+  var eventfulApiUrl = "http://api.eventful.com/json/events/search?app_key=" + EVENTFUL_API_KEY + "&where=" + lat + "," + lng + "&within=25 ";
+  $.ajax({
+    url: eventfulApiUrl
+  }).done(function(data) {
+    var eventfulResultObject = JSON.parse(data);
+    var events = eventfulResultObject.events.event;
+    //var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < events.length; i++) {
+      (function(event) {
+            var infowindow = new google.maps.InfoWindow({
+              content: event.title + "<p>" + event.venue_name
+            });
+            console.log(event.latitude + "," + event.longitude);
+            var marker = new google.maps.Marker({
+                position: {lat: parseInt(event.latitude), lng: parseInt(event.longitude)},
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: makeMarkerIcon("3373FF")
+            });
+              marker.addListener('click', function() {
+                infowindow.open(map, marker);
+              });
+              //bounds.extend(marker.position);
+            })(events[i]);
+    }
+    //map.fitBounds(bounds);
+  });
+}
 function getFoursquareInformationForLocation(lat, lng, map) {
     var foursquareApiUrl = "https://api.foursquare.com/v2/venues/search?ll=" + lat + "," + lng + "&client_id=" + FOURSQUARE_CLIENT_ID + "&client_secret=" + FOURSQUARE_CLIENT_SECRET + "&v=" + FOURSQUARE_API_VERSION;
     $.ajax({
@@ -21,22 +52,33 @@ function getFoursquareInformationForLocation(lat, lng, map) {
   .done(function( data ) {
     if ( console && console.log ) {
         var venues = data.response.venues;
-        var venue;
+       
+        
         var bounds = new google.maps.LatLngBounds();
         for (var i = 0; i < venues.length; i++) {
-            venue = venues[i];
-            console.log(venue);
+            
+            (function(venue) {
+              var category = "";
+              for (var j = 0; j < venue.categories.length; j++) {
+                category += venue.categories[j].name + " ";
+                
+              }
+              console.log(category);
             var infowindow = new google.maps.InfoWindow({
-              content: venue.name
+              content: venue.name + "<p>" + category
             });
             var marker = new google.maps.Marker({
                 position: {lat: venue.location.lat, lng: venue.location.lng},
-                map: map
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: makeMarkerIcon("FF33F2")
             });
-            marker.addListener('click', function() {
-              infowindow.open(map, marker);
-            });
-            bounds.extend(marker.position);
+              marker.addListener('click', function() {
+                infowindow.open(map, marker);
+              });
+              bounds.extend(marker.position);
+            })(venues[i]);
+            
         }
         map.fitBounds(bounds);
     }
@@ -117,6 +159,20 @@ function getFoursquareInformationForLocation(lat, lng, map) {
             }
           });
           map.fitBounds(bounds);
-               getFoursquareInformationForLocation(places[0].geometry.location.lat(), places[0].geometry.location.lng(), map);
+               //getFoursquareInformationForLocation(places[0].geometry.location.lat(), places[0].geometry.location.lng(), map);
+               getEventfulEvents(places[0].geometry.location.lat(),places[0].geometry.location.lng(), map);
         });
+      }
+// This function takes in a COLOR, and then creates a new marker
+      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+      // of 0, 0 and be anchored at 10, 34).
+      function makeMarkerIcon(markerColor) {
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+          new google.maps.Size(21, 34),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(10, 34),
+          new google.maps.Size(21,34));
+        return markerImage;
       }
